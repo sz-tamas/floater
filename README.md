4d7dacba3254248385bf32affdb19d974dccebcd
Szalczinger Tamás <szalczinger.tamas@minicrm.hu>
2017-02-26 11:31:03 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# floater
+# floater v1.0
+
 Super smooth &amp; easy jquery floater on scroll, based on sticky-kit.
+
+## Options
+
+Options can be set on the element where we want floater to bind to.
+
+**data-component="floater"**
+
+Binding floater to element.
+
+**data-floater-container**: CSS Selector
+
+This is the container of the element within we want the element to be floating on scroll.
+ 
+**data-floater-parent**: CSS Selector
+
+This is the relative parent to the element where we want the element to stick to when scroll.
+
+*Optional:* If not set it is always the floating element's parent.
+
+**data-floater-options**: JSON
+ 
+   - animate: "true","false"       # Sets element top animated or not
+   - paddingTop: 0                 # Adds a padding to the element offsettop calc when setting top
+   - paddingBottom: 0              # Adds a padding to the element offsetbottom calc when setting top
+
+## USAGE EXAMPLE
+
+Install dependencies:
+```
+$ npm i
+```
+
+In this example we want floater to stick to relative parent (*floater-wrapper*) or the window.pageYOffset if scrolled down & parent is not visible. Floating element will be also stick to the bottom of the window or relative parent bottom if height is larger then window.innerHeight.   
+
+Floating element will not float over the top or bottom of the relative parent or the content, it uses absolutie positioning to make sure it stick with parent position.
+
+```
+<section class="content">
+    <div class="sidebar">
+        <div class="floater-wrapper">
+            <div class="floater"
+                 data-component="floater"
+                 data-floater-container=".content"
+                 data-floater-parent=".floater-wrapper">
+                <div>
+                    floating content
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="maincontent">
+        fixed content
+    </div>
+</section>
+```
+
+**Multiple floaters**   
+
+Because of floater uses absolute positioning it is up to the layout and our choice what element we want to be floated, so if we need we can make two element to be floating next to each other if they have their relative parent next to each other also. (see "multiplefloatwithdynamiccontent&fixednavbar2.html" for example)
---
+++ src/floater.js
@@ -0,0 +1,112 @@
+/**
+ @license Floater v1.0 | Tamas Szalczinger
+ */
+!function($, window, namespace) {
+
+    var ns = window[namespace] || {};
+    window[namespace] = ns;
+
+    var paddingTop = 8,
+        paddingBottom = 8,
+        debug = false;
+
+    ns.Floater = (function () {
+        function Floater($element, $containerParent, $relativeParent, options) {
+            this.$element = $element;
+            this.$containerParent = $containerParent;
+            this.$relativeParent = $relativeParent.length ? $relativeParent : $element.parent();
+            this.lastScroll = 0;
+            this.scrollDirection = '';
+            this.options = $.extend({
+                paddingTop: paddingTop,
+                paddingBottom: paddingBottom
+            }, options || {});
+
+            this.init();
+            if (debug) console.log('FLOATER ATTACHED', this.$element, this.options);
+        }
+
+        Floater.attach = function(target) {
+            var self = this;
+
+            $(target).each(function(i, element) {
+               new self($(element),
+                   $($(element).data('floaterContainer')),
+                   $($(element).data('floaterParent')),
+                   $(element).data('floaterOptions')
+               );
+            });
+        };
+
+        Floater.prototype.init = function () {
+            this.$relativeParent.css({height: this.$element.height()});
+            this.$element.css({top: 0, position: 'absolute', width: 'inherit'});
+            $(window).on('scroll', this.onScroll.bind(this));
+            this.onScroll();
+        };
+
+        Floater.prototype.onScroll = function() {
+            this.scrollDirection = $(window).scrollTop() < this.lastScroll ? 'UP' : 'DOWN';
+            this.lastScroll = $(window).scrollTop();
+
+            if (debug) console.log('FLOATER SCROLL', this.scrollDirection);
+
+            this.recalc();
+        };
+
+        Floater.prototype.recalc = function () {
+            var top, oldTop = +this.$element.css('top').replace('px', ''),
+                max = this.$containerParent.height() - this.$element.height(),
+                parentOffsetTop = this.$relativeParent.offset().top,
+                offsetTop = this.$element.offset().top,
+                offsetBottom = offsetTop + this.$element.height(),
+                scrollYHeight = window.pageYOffset + window.innerHeight;
+
+            // (Re)Set relative parent height
+            this.$relativeParent.css({height: this.$element.height()});
+
+            // Stick to top
+            top = $(window).scrollTop() - parentOffsetTop + +this.options.paddingTop;
+
+            if (this.$element.height() > window.innerHeight) {
+                switch (this.scrollDirection) {
+                    case 'UP': // Stick to top || oldTop
+                        top = (offsetTop > window.pageYOffset) ?
+                            Math.min(top, oldTop) : oldTop;
+                        break;
+                    case 'DOWN': // Stick to bottom || oldTop
+                        top = (offsetBottom < scrollYHeight) ?
+                        oldTop + (scrollYHeight - offsetBottom) - +this.options.paddingBottom : oldTop;
+                        break;
+                }
+            }
+
+            top = Math.max(top, 0);
+            top = Math.min(top, max);
+
+            if (debug) console.log('FLOATER TOP', top);
+
+            window.requestAnimationFrame(function() {
+                this.options.animate === "false" ?
+                    this.scrollTop(top) :
+                    this.animateTop(top);
+            }.bind(this));
+        };
+
+        Floater.prototype.scrollTop = function(top) {
+            this.$element.css({top: top + 'px'});
+        };
+
+        Floater.prototype.animateTop = function(top) {
+            this.$element.animate({top: top + "px"}, {
+                queue: false, duration: +this.options.animationDuration || 250
+            });
+        };
+
+        return Floater;
+    })();
+
+    $(document).ready(function() {
+        ns.Floater.attach('[data-component="floater"]');
+    });
+}(jQuery, window, 'floater');
